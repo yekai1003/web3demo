38738cc31a93d2d31b2d55a003103d379d9c3a8b
yekai1003 <gaoye@tokenpai.com>
2019-11-12 10:42:45 +0800

    new web3

---
+++ abi.js
@@ -0,0 +1,31 @@
+//abi代码定义-从合约中拷贝而来
+abi = [
+   {
+      "constant": false,
+      "inputs": [
+         {
+            "name": "_msg",
+            "type": "string"
+         }
+      ],
+      "name": "setMsg",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+   },
+   {
+      "constant": true,
+      "inputs": [],
+      "name": "getMsg",
+      "outputs": [
+         {
+            "name": "",
+            "type": "string"
+         }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+   }
+]
